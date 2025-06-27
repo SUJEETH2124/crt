da34849943e7ca74b01fcf3497a7becf53d9c84c
SUJEETH2124 <kumarsujeeth796@gmail.com>
2025-06-27 15:40:22 +0530

    Create Create-Append-Remove.js

---
+++ Create-Append-Remove.js
@@ -0,0 +1,6 @@
+let aish=document.createElement("p");
+aish.textContent="I'm Chitti";
+aish.style.color="blue";
+aish.style.fontsize="20px";
+document.getElementById("vasi").appendChild(aish);
+document.body.style.backgroundColor="cyan";
