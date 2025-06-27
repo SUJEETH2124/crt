a8c30fc73d93089f344508b17b01bd70b79b574d
SUJEETH2124 <kumarsujeeth796@gmail.com>
2025-06-27 15:40:51 +0530

    27-06

--- Create-Append-Remove.js
+++ Create-Append-Remove.js
@@ -3,4 +3,15 @@ aish.textContent="I'm Chitti";
 aish.style.color="blue";
 aish.style.fontsize="20px";
 document.getElementById("vasi").appendChild(aish);
-document.body.style.backgroundColor="cyan";
+document.body.style.backgroundColor = "pink";
+setTimeout(() => {
+    let re = document.getElementsByClassName("list")[4];
+    if (re) {
+        re.remove();
+    }
+}, 2000);
+let newHeading = document.createElement("h2");
+newHeading.innerHTML = "This is a new heading added by JavaScript";
+newHeading.style.color="purple";
+newHeading.style.fontSize="20px";
+ 
